eaebc05718dec79b9aed12bab7d7b8309347a5a0
astrunk87 <134554632+astrunk87@users.noreply.github.com>
2023-09-01 12:15:31 -0500

    fixed bug where it would not push the post correctly, needed express json not static

--- Develop/public/server.js
+++ Develop/public/server.js
@@ -1,59 +1,68 @@
 const express = require('express');
-const path = require('path');
+// const path = require('path');
 const notes = require('./db/notes')
 const uuid = require('./db/uuid');
 
 const app = express();
 const PORT = 3001;
 
-app.use(express.static('public'));
+app.use(express.json());
+// app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/index.html'))
 );
 
-app.get('/', (req, res) => res.send('Navigate to /send or /routes'));
+// app.get('/', (req, res) => res.send('Navigate to /send or /routes'));
 
 // app.get('/db', (req, res) =>
 //   res.sendFile(path.join(__dirname, 'public/db.json'))
 // );
 
-app.get('/api/notes', (req, res) => res.json(notes));
+app.get('/api/notes', (req, res) => {
 // ^ this brings back the db file in json
+console.info(`${req.method} request received to get reviews`);
+
+  // Sending all reviews to the client
+  return res.json(notes);
+});
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
   });
+  // ^brings back html file when any unknown path is put in
   
 app.get('/db', (req, res) => res.json(notes));
+// ^brings back notes when db path is requested
 
 
 app.post('/api/notes', (req, res) => {
     // Let the client know that their POST request was received
     console.info(`${req.method} request received to add a review`);
+    // res.json(`${req.method} request received to add a review`);
 
-    const { title, text} = req.body;
+  // // Log our request to the terminal
+  // console.info(`${req.method} request received to add a review`);
 
-    if (title && text) {
-      // Variable for the object we will save
-      const newNote = {
-        title,
-        text,
-        note_id: uuid(),
-      };
-     
-  
-      const response = {
-        status: 'success',
-        body: newNote,
-      };
-  
-      console.log(response);
-      res.status(201).json(response);
-    } else {
-      res.status(500).json('Error in posting note');
-    }
+  let response;
+
+  // Check if there is anything in the response body
+  if (req.body && req.body.title) {
+    response = {
+      status: 'success',
+      data: req.body,
+      title: req.body.title,
+      text: req.text
+    };
+    res.json(`Review for ${response.title} has been added!`);
+  } else {
+    res.json('Request body must at least contain a product name');
+  }
+
+  // Log the response body to the console
+  console.log(req.body);
+
   });
 
   app.get('/api/notes', (req, res) => {
