dd96c8c5eaa324eb579fdd1fc1ddd43ecbd679eb
astrunk87 <134554632+astrunk87@users.noreply.github.com>
2023-09-07 18:25:47 -0500

    moved server file outside of public file, which is what was causing issues. fixed paths to reflect that update

--- Develop/server.js
+++ Develop/server.js
@@ -1,28 +1,25 @@
 const express = require('express');
 const path = require('path');
-const notes = require('./db/notes')
-const uuid = require('./helpers/uuid');
+const notes = require('./public/db/notes')
+const uuid = require('./public/helpers/uuid');
 const fs = require('fs');
-const { readFromFile, readAndAppend } = require('./helpers/fsUtils');
+const { readFromFile, readAndAppend } = require('./public/helpers/fsUtils');
 
 const app = express();
 const PORT = 3001;
 
 app.use(express.json());
 app.use(express.static('public'));
-// ^this caused all my post problems :(
+
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+  res.sendFile(path.join(__dirname, './public/index.html'))
 );
 
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/notes.html'))
+  res.sendFile(path.join(__dirname, './public/notes.html'))
 ); 
-
-
-
 
 app.get('/db', (req, res) =>{
 console.info(`${req.method} request received to get notes`);
@@ -34,10 +31,8 @@ return res.json(notes);
 app.get('/api/notes', (req, res) => {
 // ^ this brings back the db file in json
 console.info(`${req.method} request received to get notes`);
-readFromFile('./db/notes.json').then((data) => res.json(JSON.parse(data)));
+readFromFile('./public/db/notes.json').then((data) => res.json(JSON.parse(data)));
 
-  // Sending all notes to the client
-  // return res.json(notes);
 });
 
 app.get('*', (req, res) => {
@@ -50,10 +45,7 @@ app.get('*', (req, res) => {
 //   return res.json(notes)
 // });
 // ^brings back notes when db path is requested
- app.get('/api/notes', (req, res) => {
-    console.info(`GET /api/notes`);
-    res.status(200).db.json(data);
-  });
+ 
 
 app.post('/api/notes', (req, res) => {
     // Let the client know that their POST request was received
@@ -69,17 +61,7 @@ app.post('/api/notes', (req, res) => {
       note_id: uuid(),
     };
 
-    readAndAppend(newNote, './db/notes.json');
-
-    // const noteString = JSON.stringify(newNote);
-
-    // fs.appendFile(`./db/notes.js`, noteString, (err) =>
-    //   err
-    //     ?console.error(err)
-    //     : console.log(
-    //       `new note ${newNote.title} has been saved`
-    //     )
-    // );
+    readAndAppend(newNote, './db/notes.json');   
 
     const response = {
       status: 'success',
@@ -91,19 +73,7 @@ app.post('/api/notes', (req, res) => {
     res.status(400).json(`error in saving note`);
   }
 });
-  //   res.json(`Note title:${response.title}. Note text:${response.text}`);
-  // } else {
-  //   res.json('Request body must at least contain note title');
-  // }
-
-  // // Log the response body to the console
-  // console.log(req.body);
-
-  // });
-
- 
-
-  
+   
 
 
 app.listen(PORT, () =>
