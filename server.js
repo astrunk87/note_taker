499d3339640c51cf211532743195005f691d180c
astrunk87 <134554632+astrunk87@users.noreply.github.com>
2023-09-09 13:59:57 -0500

    added delete route with help from tutor. plus a few other fixes with their help

--- server.js
+++ server.js
@@ -1,57 +1,55 @@
-const express = require('express');
-const path = require('path');
-const notes = require('./db/notes.json')
-const uuid = require('uuid');
-const fs = require('fs');
-const { readFromFile, readAndAppend } = require('./helpers/fsUtils');
+const express = require("express");
+const path = require("path");
+const notes = require("./db/notes.json");
+const uuid = require("uuid");
+const fs = require("fs");
+const { readFromFile, readAndAppend, writeToFile } = require("./helpers/fsUtils");
 
 const app = express();
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, './public/index.html'))
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "./public/index.html"))
 );
 
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, './public/notes.html'))
-); 
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "./public/notes.html"))
+);
 
-app.get('/db', (req, res) =>{
-console.info(`${req.method} request received to get notes`);
+app.get("/db", (req, res) => {
+  console.info(`${req.method} request received to get notes`);
 
-// Sending all notes to the client
-return res.json(notes);
+  // Sending all notes to the client
+  return res.json(notes);
 });
 
-app.get('/api/notes', (req, res) => {
-// ^ this brings back the db file in json
-console.info(`${req.method} request received to get notes`);
-readFromFile('./db/notes.json').then((data) => res.json(JSON.parse(data)));
-
+app.get("/api/notes", (req, res) => {
+  // ^ this brings back the db file in json
+  console.info(`${req.method} request received to get notes`);
+  readFromFile("./db/notes.json").then((data) => res.json(JSON.parse(data)));
 });
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-  });
-  // ^brings you to main page when any unknown path is put in
-  
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+// ^brings you to main page when any unknown path is put in
+
 // app.get('/db', (req, res) => {
 //   console.info(`${req.method} request received to get notes`);
 //   return res.json(notes)
 // });
 // ^brings back notes when db path is requested
- 
 
-app.post('/api/notes', (req, res) => {
-    // Let the client know that their POST request was received
-    console.info(`${req.method} request received to add a note`);
-    
-  const { title, text} = req.body;
+app.post("/api/notes", (req, res) => {
+  // Let the client know that their POST request was received
+  console.info(`${req.method} request received to add a note`);
+
+  const { title, text } = req.body;
 
   // Check if there is anything in the response body
   if (text && title) {
@@ -61,23 +59,28 @@ app.post('/api/notes', (req, res) => {
       id: uuid.v4(),
     };
 
-    readAndAppend(newNote, './db/notes.json');   
+    readAndAppend(newNote, "./db/notes.json");
 
     const response = {
-      status: 'success',
+      status: "success",
       body: newNote,
     };
     console.log(response);
     res.status(201).json(response);
-  }else{
+  } else {
     res.status(400).json(`error in saving note`);
   }
 });
-   
+
+app.delete('/api/notes/:id', (req, res) => {
+    const id = req.params.id
+    const updatedNotes = notes.filter((note) => note.id !== id)
+    writeToFile("./db/notes.json", updatedNotes );
+    res.status(200).json("note deleted")
+})
+// ^ with help from tutor
 
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
 );
-
-
